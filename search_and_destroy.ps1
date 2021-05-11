c41629faa2a95894bc9081b34432ffa47a3d0855
thock2 <tjhockless@live.com>
2021-05-11 17:03:39 -0500

    Updates 5/11

--- search_and_destroy.ps1
+++ search_and_destroy.ps1
@@ -1,27 +1,26 @@
-# Search for a phishing message in all mailboxes and remove what is found.
+[CmdletBinding()]
+param (
+    [Parameter(ValueFromPipeline = $true,
+        Mandatory = $true)]
+    [Alias('blocklist')]
+    [string[]]$addresses
+)
 # From https://docs.microsoft.com/en-us/microsoft-365/compliance/search-for-and-delete-messages-in-your-organization?view=o365-worldwide
-# Modified to use sender's address instead of subject line content
+# Modified to use sender's address instead of subject line content, and also to process multiple addresseses at a time
 
 #Install-Module -Name ExchangeOnlineManagement
 #Import-Module ExchangeOnlineManagement
 
-$credential = Get-credential 'admin@yourdomain.onmicrosoft.com'
+Connect-IPPSSession
 
-Connect-IPPSSession -credential $credential
-$search_name = Read-Host "Name of search?"
-$sender_address = Read-Host "Sender address?"
+foreach ($address in $addresses) {
+    $search_name = $address + " " + "Removal"
+    $search = New-ComplianceSearch -Name $search_name -ExchangeLocation All -ContentMatchQuery "(From:$address)"
+    Start-ComplianceSearch -Identity $search.identity
+    Write-Host "Waiting 5 minutes to allow the Search to complete. Please do not close this window" -ForegroundColor Green -BackgroundColor Yellow
+    Start-Sleep -Seconds 300
+    New-ComplianceSearchAction -SearchName $search_name -Purge -PurgeType HardDelete -Confirm:$false
 
-$Search = New-ComplianceSearch -Name $search_name -ExchangeLocation All -ContentMatchQuery "(From:$sender_address)"
-Start-ComplianceSearch -Identity $Search.Identity
-
-Write-Host "Waiting 5 minutes to allow the Search to complete. Please do not close this window" -ForegroundColor Green -BackgroundColor Yellow
-Start-Sleep -Seconds 300
-
-New-ComplianceSearchAction -SearchName $search_name -Purge -PurgeType HardDelete
-
-#Show Progress of Search/Delete Action
-$purge_name = $search_name + "_Purge"
-Get-ComplianceSearchAction -Identity $purge_name -Details
+}
 
 Disconnect-ExchangeOnline -Confirm:$false
-
