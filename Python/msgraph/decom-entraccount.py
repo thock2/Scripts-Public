30e1b34bc57e46260144dfc743191f85424c0bce
thock2 <tjhockless@live.com>
2024-07-02 16:10:46 -0500

    7.2.24

--- Python/msgraph/decom-entraccount.py
+++ Python/msgraph/decom-entraccount.py
@@ -3,17 +3,18 @@ import argparse
 
 # Create / parse argument
 argument = argparse.ArgumentParser()
-argument.add_argument("email_address", help="Provide e-mail address of user")
+argument.add_argument("email_address", help="Provide e-mail address of user", nargs="*")
 arguments = argument.parse_args()
 
-# Disable account
-msgraph.disable_azureaccount("testuser@example.com")
-# Remove License
-msgraph.remove_licenses("testuser@example.com")
-# E-mail IT confirming completion
-msgraph.send_email(
-    "no-reply@example.xom)",
-    "it@example.com",
-    f"User Deactivation - {argument.email_address}",
-    f"{argument.email_address} has been deactivated",
-)
+for user in arguments.email_address:
+    # Disable account
+    print(msgraph.disable_azureaccount(user))
+    # Remove License
+    msgraph.remove_licenses(user)
+    # E-mail IT confirming completion
+    msgraph.send_email(
+        "it@example.com",
+        "testuser@example.com",
+        f"User Deactivation - {user}",
+        f"{user} has been deactivated",
+    )
